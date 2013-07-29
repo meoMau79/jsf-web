1ac1bc18cc879cfbf18fb44330029022f5b20396
Jason Faulkner <scientific.feelings@gmail.com>
2013-07-29 11:02:17 -0400

    addition of isotope ux

---
+++ javascripts/sextoy.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function ($) {
+
+
+
+
+var $container = $('#container');
+
+$('#filters a').click(function(){
+	
+	var selector = $(this).attr('data-filter');
+	$container.isotope({filter:selector, layoutMode:'cellsByRow'});
+	return false;
+	
+});
+
+
+$('.portfolio').on('mouseover', function(){
+	$(this).find('.cover').show();
+
+})
+
+$('.portfolio').on('mouseout', function(){
+	$(this).find('.cover').hide();
+
+})
+
+	
+
+
+});
